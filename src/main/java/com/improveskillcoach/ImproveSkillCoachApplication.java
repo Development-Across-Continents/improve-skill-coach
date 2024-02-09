cc3bad2ac901d61655620af2edfa7fe5761d6270
gil-son <gilszon@gmail.com>
2024-02-09 20:42:24 -0300

    Inital

---
+++ src/main/java/com/improveskillcoach/ImproveSkillCoachApplication.java
@@ -0,0 +1,13 @@
+package com.improveskillcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImproveSkillCoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImproveSkillCoachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/improveskillcoach/ImproveSkillCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.improveskillcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImproveSkillCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
